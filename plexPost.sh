cd0f0b3d512c047e85cee224da797245559d7241
Unkas <pxs2090@rit.edu>
2017-08-03 15:13:57 -0400

    Initial Commit of the plexPost.sh script

---
+++ plexPost.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPT=$(readlink -f $0)
+SCRIPTDIR=$(dirname ${SCRIPT})
+BASE=$(dirname ${SCRIPTDIR})
+QUEUEDIR=${BASE}/queue
+
+PLEXFILE="$1"
+PLEXFN=$(basename "$1")
+WORKDIR=$(dirname "$1")
+JOBID=$(basename "$WORKDIR")
+
+echo "${PLEXFN" > "${QUEUEDIR}/${JOBID}.job
